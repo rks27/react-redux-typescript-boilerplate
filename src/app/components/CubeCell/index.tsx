705a4f3f30ee9868390ae8d014ee347459bd180f
Rajesh Singh <rasingh@microsoft.com>
2018-05-11 17:59:45 -0700

    Dom ref updated

--- src/app/components/CubeCell/index.tsx
+++ src/app/components/CubeCell/index.tsx
@@ -6,7 +6,15 @@ var ctx = canvas.getContext("2d");
 import * as React from 'react';
 import { Point3D } from '../../models/CubeModel'
 
-export default class Cell extends React.Component {
+export default class Cell extends React.Component<any, any> {
+    canvas: any;
+
+    constructor(props: any) {
+        super(props);
+
+        this.canvas = null;
+    }
+   
      vertices = [
         new Point3D(-1,1,-1),
         new Point3D(1,1,-1),
@@ -26,49 +34,24 @@ export default class Cell extends React.Component {
     angle = 0;
 
     startDemo() : void {            
-      var canvas:any = document.getElementById("thecanvas");
-          if( canvas && canvas.getContext ) {
-              const ctx = canvas.getContext("2d");
-              setInterval(() => this.loop(ctx), 33);
+          if( this.canvas && this.canvas.getContext ) {
+              const ctx = this.canvas.getContext("2d");
+              setInterval(() => this.loop(ctx), 500);
           }
       }
     
-    // loop(ctx: any)  : void {      
-    //   var t = new Array();
-    //     ctx.fillStyle = "rgb(0,0,0)";
-    //     ctx.fillRect(0,0,400,200);
-    //     for( var i = 0; i < this.vertices.length; i++ ) {
-    //         var v = this.vertices[i];
-    //         var r = v.rotateX(this.angle).rotateY(this.angle).rotateZ(this.angle);
-    //         var p = r.project(400,200,128,3.5);
-    //         t.push(p)
-    //     }
-
-    //     ctx.strokeStyle = "rgb(255,55,255)"
-    //     for( var i = 0; i < this.faces.length; i++ ) {
-    //         var f = this.faces[i]
-    //         ctx.beginPath()
-    //         ctx.moveTo(t[f[0]].x,t[f[0]].y)
-    //         ctx.lineTo(t[f[1]].x,t[f[1]].y)
-    //         ctx.lineTo(t[f[2]].x,t[f[2]].y)
-    //         ctx.lineTo(t[f[3]].x,t[f[3]].y)
-    //         ctx.closePath()
-    //         ctx.stroke()
-    //     }
-    //     this.angle += 2
-    // }
-
+   
     loop(ctx:any) : void {
       var t = new Array();
 
-      ctx.fillStyle = "rgb(0,0,0)";
+      ctx.fillStyle = "rgb(200,200,200)";
       ctx.fillRect(0,0,400,250);
 
       for( var i = 0; i < this.vertices.length; i++ ) {
           var v = this.vertices[i];
-         // var r = v.rotateX(this.angle).rotateY(this.angle);
-          var p = v.rotateX(25).rotateY(25).project(400,250,200,4);
-          t.push(p)
+          var r = v.rotateX(this.angle).rotateY(this.angle);
+          //var p = r.project(200,300,700,20);
+          t.push(r)          
       }
 
       var avg_z = new Array();
@@ -83,33 +66,64 @@ export default class Cell extends React.Component {
       });
 
       for( var i = 0; i < this.faces.length; i++ ) {
+          var sy = 50
+          var sx = 50
+          var w = 100;
+          var ox = 50;
+          var oy = 50;
           var f = this.faces[avg_z[i].index]
-
           ctx.fillStyle = this.arrayToRGB(this.colors[avg_z[i].index]);
           ctx.beginPath()
-          ctx.moveTo(t[f[0]].x,t[f[0]].y)
-          ctx.lineTo(t[f[1]].x,t[f[1]].y)
-          ctx.lineTo(t[f[2]].x,t[f[2]].y)
-          ctx.lineTo(t[f[3]].x,t[f[3]].y)
-          ctx.closePath()
-          ctx.fill()
+          ctx.moveTo(ox, oy + sy); // v1
+          ctx.lineTo(ox + w, oy + sy); // v2
+          ctx.lineTo(ox + w, oy + w + sy) ; //v3
+          ctx.lineTo(ox,oy + w + sy); // v4
+        
+          
+        ctx.closePath()
+        ctx.fill()
+
+        ctx.fillStyle = "rgb(255,0,0)";
+        ctx.beginPath()
+        ctx.moveTo(ox, oy + sy); // v1
+        ctx.lineTo(ox + sx, oy); // v5
+        ctx.lineTo(ox + w + sx  , oy); // v6
+        ctx.lineTo(ox + w, sy + oy);   // v2     
+        ctx.closePath()
+        ctx.fill()
+
+        ctx.fillStyle = "rgb(0,255,0)";
+        ctx.beginPath()
+        ctx.moveTo(ox + w, sy + oy);   // v2        
+        ctx.lineTo(ox + w + sx  , oy); // v6
+        ctx.lineTo(ox + sx + w, oy + w);  // v7
+        ctx.lineTo(ox + w, oy + w + sy) ; //v3
+        
+        ctx.closePath()
+        ctx.fill()
+
+
       }
+      
       this.angle += 2
   }
 
     /* Constructs a CSS RGB value from an array of 3 elements. */
    arrayToRGB(arr: any) : string {
-      if( arr.length == 3 ) {
-          return "rgb(" + arr[0] + "," + arr[1] + "," + arr[2] + ")";
-      }
+    //   if( arr.length == 3 ) {
+    //       return "rgb(" + arr[0] + "," + arr[1] + "," + arr[2] + ")";
+    //   }
       return "rgb(0,0,0)";
+  }
+
+  componentDidMount() : void {
+    this.startDemo();     
   }
 
   render() {
     return (
-      <div>
-      <button onClick={this.startDemo.bind(this)}> Start </button>
-        <canvas id="thecanvas" width="400" height="400">             
+      <div>      
+        <canvas id="thecanvas" width="400" height="400" ref={(element) => this.canvas = element}>             
         </canvas>
       </div>
     );
